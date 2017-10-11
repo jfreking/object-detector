06005491d06b4c06a97d55ad8242e24ab2316690
jfreking <jacobfreking@gmail.com>
2017-10-11 15:57:51 -0500

    Update nms.py
    Implemented faster nms algorithm from https://www.pyimagesearch.com/2015/02/16/faster-non-maximum-suppression-python/

--- object-detector/nms.py
+++ object-detector/nms.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def overlapping_area(detection_1, detection_2):
     '''
     Function to calculate overlapping area'si
@@ -15,12 +17,16 @@ def overlapping_area(detection_1, detection_2):
     # rectangles
     x1_tl = detection_1[0]
     x2_tl = detection_2[0]
+    
     x1_br = detection_1[0] + detection_1[3]
     x2_br = detection_2[0] + detection_2[3]
+    
     y1_tl = detection_1[1]
     y2_tl = detection_2[1]
+    
     y1_br = detection_1[1] + detection_1[4]
     y2_br = detection_2[1] + detection_2[4]
+    
     # Calculate the overlapping Area
     x_overlap = max(0, min(x1_br, x2_br)-max(x1_tl, x2_tl))
     y_overlap = max(0, min(y1_br, y2_br)-max(y1_tl, y2_tl))
@@ -41,21 +47,62 @@ def nms(detections, threshold=.5):
     The output is a list of detections.
     '''
     if len(detections) == 0:
-	return []
+	    return []
+    detections = np.array(detections)
+    
+    x1 = detections[:,0]
+    y1 = detections[:,1]
+    x2 = detections[:,3]
+    y2 = detections[:,4]
+	
+    area = (x2 - x1 + 1) * (y2 - y1 + 1)
     # Sort the detections based on confidence score
-    detections = sorted(detections, key=lambda detections: detections[2],
-            reverse=True)
+    #detections = sorted(detections, key=lambda detections: detections[2],reverse=True)
+    
+    idxs = np.argsort(y2)
     # Unique detections will be appended to this list
     new_detections=[]
+    
     # Append the first detection
-    new_detections.append(detections[0])
+    #new_detections.append(detections[0])
+    
     # Remove the detection from the original list
-    del detections[0]
+    #del detections[0]
+    
     # For each detection, calculate the overlapping area
     # and if area of overlap is less than the threshold set
     # for the detections in `new_detections`, append the 
     # detection to `new_detections`.
+    
     # In either case, remove the detection from `detections` list.
+    while len(idxs) >= 0:
+        last = len(idxs)-1
+        i = idxs[last]
+        new_detections.append(i)
+        
+        # find the largest (x, y) coordinates for the start of
+		# the bounding box and the smallest (x, y) coordinates
+		# for the end of the bounding box
+        xx1 = np.maximum(x1[i], x1[idxs[:last]])
+        yy1 = np.maximum(y1[i], y1[idxs[:last]])
+        xx2 = np.minimum(x2[i], x2[idxs[:last]])
+        yy2 = np.minimum(y2[i], y2[idxs[:last]])
+        
+        # compute the width and height of the bounding box
+        w = np.maximum(0, xx2 - xx1 + 1)
+        h = np.maximum(0, yy2 - yy1 + 1)
+ 
+		# compute the ratio of overlap
+        overlap = (w * h) / area[idxs[:last]]
+ 
+		# delete all indexes from the index list that have
+        idxs = np.delete(idxs, np.concatenate(([last],np.where(overlap > threshold)[0])))
+ 
+	# return only the bounding boxes that were picked using the
+	# integer data type
+	return detections[new_detections].astype("int")
+        
+    """
     for index, detection in enumerate(detections):
         for new_detection in new_detections:
             if overlapping_area(detection, new_detection) > threshold:
@@ -65,7 +112,7 @@ def nms(detections, threshold=.5):
             new_detections.append(detection)
             del detections[index]
     return new_detections
-
+    """
 if __name__ == "__main__":
     # Example of how to use the NMS Module
     detections = [[31, 31, .9, 10, 10], [31, 31, .12, 10, 10], [100, 34, .8,10, 10]]
